e0c23b4be16125ca0cbceec98f0d727d7f4e9a7b
cgars <christian@stuebeweg50.de>
2017-04-11 16:27:30 +0200

    Set owner of ActiveAccounts view to test

--- 1_initial-schema.sh
+++ 1_initial-schema.sh
@@ -133,6 +133,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
    ALTER TABLE refreshtokens OWNER TO test;
    ALTER TABLE sessions OWNER TO test;
    ALTER TABLE sshkeys OWNER TO test;
+   ALTER VIEW activeaccounts OWNER TO test;
 
 
 EOSQL
